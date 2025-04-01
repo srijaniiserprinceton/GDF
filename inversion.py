Fixed issues with biMax MCMC fit. Weighting function was tweaked to fit 1 thermal extent to 2 thermal extents.

inversion.py
@@ -31,7 +31,7 @@ class gyrovdf:
 
         # obtaining the grid points from an actual PSP field-aligned VDF (instrument frame)
         self.fac = coor_fn.fa_coordinates()
-        self.fac.get_coors(self.vdf_dict, trange, count_mask=1, plasma_frame=True, TH=TH, CREDENTIALS=CREDENTIALS, CLIP=CLIP)
+        self.fac.get_coors(self.vdf_dict, trange, count_mask=2, plasma_frame=True, TH=TH, CREDENTIALS=CREDENTIALS, CLIP=CLIP)
 
     @profile
     def setup_new_inversion(self, tidx, knots=None, plot_basis=False, mincount=2):
@@ -384,9 +384,10 @@ def calc_gyrotropic_moments(gvdf, vdf_rec_nonan, DIMS=(101, 201)):
 
 if __name__=='__main__':
     # loading VDF and defining timestamp
+    # trange = ['2024-03-26T00:00:00', '2024-03-26T12:00:00']
     # trange = ['2020-01-29T00:00:00', '2020-01-29T23:59:59']
-    # trange = ['2020-01-26T00:00:00', '2020-01-26T23:59:59']
-    trange = ['2024-12-23T00:00:00', '2024-12-24T16:00:00']
+    trange = ['2020-01-26T00:00:00', '2020-01-26T23:59:59']
+    # trange = ['2024-12-23T00:00:00', '2024-12-23T10:00:00']
     credentials = load_config('./config.json')
     creds = [credentials['psp']['sweap']['username'], credentials['psp']['sweap']['password']]
     # creds = None
@@ -398,14 +399,14 @@ if __name__=='__main__':
     # tidx = np.argmin(np.abs(psp_vdf.time.data - np.datetime64('2020-01-29T18:10:06')))
     # tidx = np.argmin(np.abs(psp_vdf.time.data - np.datetime64('2019-04-05T20:21:36')))
     # tidx = np.argmin(np.abs(psp_vdf.time.data - np.datetime64('2020-01-26T14:10:42')))
-    tidx = 4252
-    # tidx = 4500
+    # tidx = 4252
+    tidx = 4500
     # tidx = np.argmin(np.abs(psp_vdf.time.data - np.datetime64('2024-12-24T14:08:34')))
     # tidx = 11146
     # tidx = 200
     # tidx = np.argmin(np.abs(psp_vdf.time.data - np.datetime64('2020-01-26T14:10:42')))
     # tidx = 9960
-    tidx = 9956
+    # tidx = 9956
 
     # initializing the inversion class
     gvdf = gyrovdf(psp_vdf, trange, TH=60, Lmax=12, N2D_restrict=True, CREDENTIALS=creds, CLIP=True)

super_resolution.py
@@ -13,6 +13,7 @@ class supres:
         self.data = logvdf
         self.xdata = vpara
         self.ydata = vperp
+        self.ampcore_init = np.max(self.data) + 1
 
         # to be filled in at the end of plotting
         self.biMax_fit_params = None
@@ -70,18 +71,20 @@ class supres:
 
             # mask = self.data - np.nanmax(self.data) >= -3
             # mask = np.ones_like(self.data, dtype='bool')
-            # vyth_core = 300 #vthani_core * vxth_core
+            # vyth_core = 100*np.power(10.,vthani_core) * np.power(10.,vxth_core)
             # mask = (self.ydata >= -vyth_core) * (self.ydata <= vyth_core)
             # vxth_core = 50
             # mask2 =  (self.xdata <= (vxcore - vxth_core))
-            # weight = np.ones_like(self.data)
-            # weight[(self.data <= -0.889) * (self.data >= -1.333)] += 100
+            weight = np.ones_like(self.data)
+            # weight[mask] = 100
+            weight[(self.data <= -0.678) * (self.data >= -1.737)] += 100
+            # weight[(self.data >= -1.737)] += 100
             # alpha = 10.0
             # quantile = np.quantile(residual, 0.9)
             # weight = 1.0 + alpha * np.maximum(residual, 0)**2
             # weight2 = np.where(residual > quantile, alpha, 1.0)
             # weight[mask * mask2] = alpha #* weight2[mask]
-            weight = 1 / np.sqrt(np.abs(self.ydata / 100))
+            # weight = 1 / np.sqrt(np.abs(self.ydata / 100))
             # weight[(self.data <= 0) * (self.data >= -1.5)] += 100
             # cost = np.nansum(np.power(10.,self.data)*(self.data - logbiMax)**2)
             # cost = np.nansum(np.power(10.,self.data[mask]) * (self.data[mask] - logbiMax[mask])**2)
@@ -94,7 +97,7 @@ class supres:
 
         # initializing the biMax_params
         uxcore_init = self.xdata[np.argmax(self.data)] * 1e-2  # -5
-        uxbeam_init = uxcore_init - 2
+        uxbeam_init = uxcore_init - 3
         vxth_core_init = np.log10(0.5)
         vthani_core_init = np.log10(1)
         vxth_beam_init = np.log10(0.5)
@@ -118,7 +121,7 @@ class supres:
         sampler = emcee.EnsembleSampler(nwalkers, 8, log_probability)
         sampler.run_mcmc(pos, 10000, progress=True)
 
-        flat_samples = sampler.get_chain(discard=5000, thin=15, flat=True)
+        flat_samples = sampler.get_chain(discard=8000, thin=15, flat=True)
 
         # converting the temperatures and anisotropies to linear scale
         # flat_samples[:,2:6] = np.power(10, flat_samples[:,2:6])
@@ -225,9 +228,14 @@ def synthetic_test():
 if __name__=='__main__':
     vdf_rec = np.load('vdf_Sleprec.npy').flatten()
     amp_shift = 0
-    log_vdf_rec = np.log10(vdf_rec / np.nanmax(vdf_rec)) + amp_shift
+    log_vdf_rec = np.log10((vdf_rec / np.nanmax(vdf_rec)) + 1e-4) + amp_shift
     vpara = np.load('vpara.npy').flatten()
     vperp = np.load('vperp.npy').flatten()
+
+    # mask = vpara < -350
+    # log_vdf_rec = log_vdf_rec[mask]
+    # vpara = vpara[mask]
+    # vperp = vperp[mask]
     '''
     vdf_rec, vpara, vperp = synthetic_test()
     '''