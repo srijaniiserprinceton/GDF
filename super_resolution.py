880b7b31e0413f148d7a961fba265d9224dbb7eb
srijaniiserprinceton <sbdas@princeton.edu>
2025-04-02 13:59:27 -0400

    Cleaned up the bi-Max fitting MCMC code

--- super_resolution.py
+++ super_resolution.py
@@ -9,31 +9,35 @@ import emcee, corner
 import matplotlib.pyplot as plt; plt.ion()
 
 class supres:
-    def __init__(self, logvdf, vpara, vperp):
-        self.data = logvdf
+    def __init__(self, vdf, vpara, vperp, baseline=1e-4):
+        self.data = np.log10((vdf / np.nanmax(vdf)) + baseline)
         self.xdata = vpara
         self.ydata = vperp
-        self.ampcore_init = np.max(self.data) + 1
+        # to be also used in the forward model when comparing fitted data with input data
+        self.baseline = baseline
 
         # to be filled in at the end of plotting
         self.biMax_fit_params = None
 
     def Maxwellian(self, Max_params):
+        # extracting the fitting parameters
         amp, ux, vxth, vyth = Max_params
 
+        # 100 is scaled with the velocities to make the paramters of order unity
         return np.power(10., amp) * np.exp(-((self.xdata - 100*ux) / (100*vxth))**2 - (self.ydata / (100*vyth))**2)
 
     def biMax(self, biMax_params):
         uxcore, uxbeam, vxth_core, vthani_core, vxth_beam, vthani_beam, amp_core, beam_core_ampratio = biMax_params
+        # since amplitudes are in log-scale
         amp_beam = beam_core_ampratio + amp_core
 
-        # converting from log to linear space
+        # converting thermal velocities and anisotropies from log to linear space
         vxth_core, vthani_core, vxth_beam, vthani_beam = np.power(10, (vxth_core, vthani_core, vxth_beam, vthani_beam))
 
-        # core parameters
+        # core parameters (only amplitude is still in log scale)
         Max_core = self.Maxwellian((amp_core, uxcore, vxth_core, vthani_core * vxth_core))
 
-        # beam parameters
+        # beam parameters (only amplitude is still in log scale)
         Max_beam = self.Maxwellian((amp_beam, uxbeam, vxth_beam, vthani_beam * vxth_beam))
 
         # adding the core and beam Maxwellians
@@ -62,41 +66,26 @@ class supres:
             return lp + log_likelihood(biMax_params)
 
         def log_likelihood(biMax_params):
-            vxcore, _, vxth_core, vthani_core, _, _, _, _ = biMax_params
             biMax_model = self.biMax(biMax_params)
-            logbiMax = np.log10(biMax_model + 1e-4)
-            # logbiMax = biMax_model
 
+            # adding a small baseline; 1e-4 was chosen since the lowest values were around 10^4 less than core
+            # this number can be changed later
+            logbiMax = np.log10(biMax_model + self.baseline)
+
+            # residual values between data and model fitting
             residual = self.data - logbiMax
 
-            # mask = self.data - np.nanmax(self.data) >= -3
-            # mask = np.ones_like(self.data, dtype='bool')
-            # vyth_core = 100*np.power(10.,vthani_core) * np.power(10.,vxth_core)
-            # mask = (self.ydata >= -vyth_core) * (self.ydata <= vyth_core)
-            # vxth_core = 50
-            # mask2 =  (self.xdata <= (vxcore - vxth_core))
             weight = np.ones_like(self.data)
-            # weight[mask] = 100
-            weight[(self.data <= -0.678) * (self.data >= -1.737)] += 100
-            # weight[(self.data >= -1.737)] += 100
-            # alpha = 10.0
-            # quantile = np.quantile(residual, 0.9)
-            # weight = 1.0 + alpha * np.maximum(residual, 0)**2
-            # weight2 = np.where(residual > quantile, alpha, 1.0)
-            # weight[mask * mask2] = alpha #* weight2[mask]
-            # weight = 1 / np.sqrt(np.abs(self.ydata / 100))
-            # weight[(self.data <= 0) * (self.data >= -1.5)] += 100
-            # cost = np.nansum(np.power(10.,self.data)*(self.data - logbiMax)**2)
-            # cost = np.nansum(np.power(10.,self.data[mask]) * (self.data[mask] - logbiMax[mask])**2)
-            cost = np.nansum(weight * residual**2)
-            # cost = np.nansum(np.log10(np.power(10,logbiMax[mask]) / np.power(10,self.data[mask]))**2)
+            # weight[(self.data <= -0.678) * (self.data >= -1.737)] += 100
+            weight[(self.data <= -0.678) * (self.data >= -2.71)] += 100
 
+            cost = np.nansum(weight * residual**2)
 
             return -0.5 * cost
 
 
         # initializing the biMax_params
-        uxcore_init = self.xdata[np.argmax(self.data)] * 1e-2  # -5
+        uxcore_init = self.xdata[np.argmax(self.data)] * 1e-2
         uxbeam_init = uxcore_init - 3
         vxth_core_init = np.log10(0.5)
         vthani_core_init = np.log10(1)
@@ -141,7 +130,6 @@ class supres:
     def plotfit(self):
         def Maxwellian(Max_params):
             amp, ux, vxth, vyth = Max_params
-
             return np.power(10., amp) * np.exp(-((xgrid - 100*ux) / (100*vxth))**2 - (ygrid / (100*vyth))**2)
 
         def biMax(biMax_params):
@@ -171,7 +159,7 @@ class supres:
         plt.scatter(self.ydata[~zeromask], self.xdata[~zeromask], c='k', s=2)
         plt.tight_layout()
 
-        # plotting the contour of the fit over the data
+        # plotting the fitted bi-Maxwellian
         x = np.linspace(np.nanmin(self.xdata), np.nanmax(self.xdata), 120)
         y = np.linspace(-np.nanmax(np.abs(self.ydata)), np.nanmax(np.abs(self.ydata)), 101)
         xgrid, ygrid = np.meshgrid(x, y, indexing='ij')
@@ -182,68 +170,18 @@ class supres:
         plt.contourf(ygrid, xgrid, biMax_fit_values - amp_shift, cmap='jet', levels=np.linspace(-4,0,10), alpha=0.7)
         plt.colorbar()
         plt.scatter(self.ydata[~zeromask], self.xdata[~zeromask], c='k', s=2)
-        plt.tight_layout()
-
-    '''
-    def makeCollars(self, biMax_params, valfven, vpara, vperp):
-
-
-    def get_superres_points(self, vpara, vperp):
-    '''
-
-def synthetic_test():
-    # initializing the biMax_params
-    uxcore = -3
-    uxbeam = uxcore -1.7
-    vxth_core = 0.5
-    vthani_core = 1.0
-    vxth_beam = 0.5
-    vthani_beam = 1.0
-    amp_core = 0
-    beam_core_ampratio = -2
-
-    biMax_params = (uxcore, uxbeam, vxth_core, vthani_core,
-                    vxth_beam, vthani_beam, amp_core, beam_core_ampratio)
-
-    # generating a grid
-    x, y = np.linspace(-1000, 0, 120), np.linspace(-400, 400, 101)
-    xx, yy = np.meshgrid(x, y, indexing='ij')
-
-    # generating the data
-    genvdf = supres(None, xx.flatten(), yy.flatten(), 1)
-    data = genvdf.biMax(biMax_params)
-
-    data = data / np.nanmin(data)
-
-    data_reshaped = np.reshape(data, (len(x), len(y)))
-
-    plt.figure()
-    plt.contourf(yy, xx, np.log10(data_reshaped), cmap='jet', levels=12)
-    plt.colorbar()
-    plt.tight_layout()
-
-    return data, xx.flatten(), yy.flatten()
-    
+        plt.tight_layout()   
 
 if __name__=='__main__':
     vdf_rec = np.load('vdf_Sleprec.npy').flatten()
     amp_shift = 0
-    log_vdf_rec = np.log10((vdf_rec / np.nanmax(vdf_rec)) + 1e-4) + amp_shift
     vpara = np.load('vpara.npy').flatten()
     vperp = np.load('vperp.npy').flatten()
 
-    # mask = vpara < -350
-    # log_vdf_rec = log_vdf_rec[mask]
-    # vpara = vpara[mask]
-    # vperp = vperp[mask]
-    '''
-    vdf_rec, vpara, vperp = synthetic_test()
-    '''
+    # initializing the super-resolution class
+    supres_vdf = supres(vdf_rec, vpara, vperp)
 
-    supres_vdf = supres(log_vdf_rec, vpara, vperp)
-    # supres_vdf = supres(np.log10(vdf_rec), vpara, vperp, np.nanmax(vdf_rec))
-
-    # performing a biMax fitting
+    # performing a biMax fitting to obtain the collars
     bimax_best_params, biMax_lower_params, biMax_upper_params = supres_vdf.biMaxfit()
 
     # plotting the fitted biMax VDF over the Slepian reconstructed data
