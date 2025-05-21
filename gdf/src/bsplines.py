e95a2cd0612c64e1627a89938ac8f1edd1336e55
srijaniiserprinceton <srijanbdas@alumni.princeton.edu>
2025-05-21 15:47:57 -0400

    Moved bsplines.py to gdf/src

---
+++ gdf/src/bsplines.py
@@ -0,0 +1,52 @@
+import numpy as np
+import bspline
+import bspline.splinelab as splinelab
+import matplotlib.pyplot as plt; plt.ion()
+
+class bsplines:
+    def __init__(self, knots, p=3):
+        self.knots = knots
+        self.p = p
+        self.gen_bsp_basis()
+
+    def gen_bsp_basis(self):
+        self.knots_aug = splinelab.augknt(self.knots, self.p)  # add endpoint repeats as appropriate for spline order p
+        self.B = bspline.Bspline(self.knots_aug, self.p)            # create spline basis of order p on knots k
+
+    def eval_bsp_basis(self, x):
+        # bsp = np.array([self.B(i) for i in x]).T # <------ these are the basis functions
+        bsp = self.B.collmat(x).T                  # <------ these are the basis functions
+        # making a small adjustment in the right most B-spline
+        bsp[-1,-1] = bsp[0,0]
+
+        return bsp
+
+if __name__=='__main__':
+    p = 3              # order of spline (as-is; 3 = cubic)
+    nknots = 20        # number of knots to generate (here endpoints count only once)
+    knots = np.linspace(0,1,nknots)  # create a knot vector without endpoint repeats
+
+    bsp = bsplines(knots, p)
+    
+    x_min = np.min(bsp.knots)
+    x_max = np.max(bsp.knots)
+    x = np.linspace(x_min, x_max, num=1000)
+    bsp_basis = bsp.eval_bsp_basis(x)
+
+    # plotting the B-spline components
+    for b in bsp_basis:
+        plt.plot(x,b)
+    plt.title('B-spline basis elements')
+
+    # trying to create Bsplines using scipy
+    from scipy.interpolate import BSpline
+    t = np.array([knots[0] for i in range(p)])
+    t = np.append(t, knots)
+    t = np.append(t, np.array([knots[-1] for i in range(p)]))
+    c = np.identity(nknots + (p-1))
+    spl = BSpline(t, c, p)
+    x = np.linspace(0,1,1000)
+
+    # plotting the B-spline components
+    plt.figure()
+    plt.plot(x, spl(x))
