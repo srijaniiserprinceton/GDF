f2ac4a87eeae87d083ee5acd9333d1c2ac6a1cd9
TerresM <michael.terres@cfa.harvard.edu>
2025-06-09 14:36:39 -0400

    modified bimax fit ubulk

--- init_gdf.py
+++ init_gdf.py
@@ -3,23 +3,24 @@
 # Define the time range we are running for
 # TRANGE = ['2020-01-26T14:28:00', '2020-01-26T15:30:59']
 # TRANGE = ['2022-02-25T16:56:00', '2022-02-25T17:01:00']
-TRANGE = ['2024-03-30T12:12:00', '2024-03-30T17:30:59']
+# TRANGE = ['2024-03-30T12:12:00', '2024-03-30T17:30:59']
+TRANGE = ['2018-11-07T03:30:00', '2018-11-07T03:55:00']
 # TRANGE = ['2024-09-24T12:12:00', '2024-09-24T17:30:59']
 
 # Get the credentials
 CREDS = None       # credential file.
 
 # Start and End index
-START_INDEX = 15
-NSTEPS = 1 #None
+START_INDEX = 0
+NSTEPS = None
 
 # Core analysis parameters
 TH                = 30
-LMAX              = 32
+LMAX              = 12
 N2D               = 3
 P                 = 3
 SPLINE_MINCOUNT   = 14
-COUNT_MASK        = 1
+COUNT_MASK        = 2
 MU                = 1e-3   # only used for gyroaxis and not superresolution
 CLIP              = True
 NPTS_SUPER        = 101
