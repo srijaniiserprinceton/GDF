976fade409bc789f2d0f865c0b6e652b1d93eeaf
TerresM <michael.terres@cfa.harvard.edu>
2025-06-12 18:55:40 -0400

    Adjusting the bimax models

--- init_gdf.py
+++ init_gdf.py
@@ -11,22 +11,22 @@ TRANGE = ['2018-11-07T03:30:00', '2018-11-07T03:55:00']
 CREDS = None       # credential file.
 
 # Start and End index
-START_INDEX = 0
-NSTEPS = 1 #None
+START_INDEX = 9
+NSTEPS = 1#None
 
 # Core analysis parameters
 TH                = 30
 LMAX              = 12
 N2D               = 3
 P                 = 3
-SPLINE_MINCOUNT   = 7
+SPLINE_MINCOUNT   = 14
 COUNT_MASK        = 2
 MU                = 1e-3   # only used for gyroaxis and not superresolution
 CLIP              = True
 NPTS_SUPER        = 101
 MCMC              = True
 MCMC_WALKERS      = 8
-MCMC_STEPS        = 2000
+MCMC_STEPS        = 200
 MIN_METHOD        = 'L-BFGS-B'
 SAVE_FIGS         = True
 SAVE_PKL          = True
