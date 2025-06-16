1de2fdc7a113278a3fabb345be420d22600b2c45
srijaniiserprinceton <srijanbdas@alumni.princeton.edu>
2025-06-16 11:51:26 -0400

    Added multiple .rst as skeleton for the Github pages of GDF

---
+++ docs/source/basis_functions.rst
@@ -0,0 +1,5 @@
+.. _basis_functions:
+
+Discription of basis functions
+==============================
+Under development.
---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'GDF'
+copyright = '2025, Srijan Bharati Das, Michael Terres'
+author = 'Srijan Bharati Das, Michael Terres'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.duration',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
