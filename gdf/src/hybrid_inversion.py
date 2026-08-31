Fixed incorrect hybrid masking. Previous mask applied to incorrect dimensions, Fix: required building the 2D grid of v_paras and then defining mask and flattening.

gdf/src/hybrid_inversion.py
@@ -282,9 +282,14 @@ def super_resolution(gvdf_tstamp, tidx, NPTS):
     hybrid_dict['Af_full'] = hybrid_dict['Af'] * 1.0
     hybrid_dict['Bf_full'] = hybrid_dict['Bf'] * 1.0
 
-    vparas = np.abs(gvdf_tstamp.grid_points[:,1])
-    mask = (vparas <  np.min(vparas)+ 50)
-    # mask = np.ones_like(vparas, dtype=bool)
+    raw_vparas = np.abs(gvdf_tstamp.grid_points[:,1])
+    
+    grid_dim = int(np.sqrt(hybrid_dict['nf']))
+    vparas_2d = np.reshape(raw_vparas, (grid_dim, grid_dim))
+    vparas_transposed = np.transpose(vparas_2d).flatten()
+
+    # Create the mask using the properly aligned array
+    mask = (vparas_transposed > np.min(vparas_transposed) + 100)
 
     hybrid_dict['mask'] = mask
     hybrid_dict['Af'] = hybrid_dict['Af'][mask,:]

gdf/src/plotter.py
@@ -410,9 +410,8 @@ import astropy.units as u
 def hybrid_plotter(gvdf_tstamp, vdf_inv, vdf_super, tidx,
                    model_misfit=None, data_misfit=None, GRID=True, SAVE_FIGS=False, ext='png'):
     vdf_super_polcap, vdf_super_cartesian = vdf_super
-    print(gvdf_tstamp.qtn_data)
-    if gvdf_tstamp.qtn_data is not None:
-        VA = ((np.linalg.norm(gvdf_tstamp.b_span[tidx]) * u.nT)/np.sqrt(c.mu0 * c.m_p * np.nanmean(gvdf_tstamp.qtn_data.electron_density) * u.cm**(-3))).to(u.km/u.s).value
+    if gvdf_tstamp.qtn_electron is not None:
+        VA = ((np.linalg.norm(gvdf_tstamp.b_span[tidx]) * u.nT)/np.sqrt(c.mu0 * c.m_p * np.nanmean(gvdf_tstamp.qtn_electron) * u.cm**(-3))).to(u.km/u.s).value
     else:
         VA = ((np.linalg.norm(gvdf_tstamp.b_span[tidx]) * u.nT)/np.sqrt(c.mu0 * c.m_p * np.nanmean(gvdf_tstamp.rec_quants['dens'][tidx,0]) * u.cm**(-3))).to(u.km/u.s).value
 
@@ -530,7 +529,7 @@ def context_axis_plot(ax3, gvdf_tstamp, tidx):
     colors = ['black', 'red', 'green']
 
     # plotting the QTN densities if available
-    if(gvdf_tstamp.qtn_data is not None):
+    if(gvdf_tstamp.qtn_electron is not None):
         inset_axes_list[0].plot(gvdf_tstamp.qtn_electron,
                                 gvdf_tstamp.qtn_electron, 'xb', alpha=0.5)
 